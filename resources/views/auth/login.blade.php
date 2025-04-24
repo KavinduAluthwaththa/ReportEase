116ebcb314b9e25e173448b54bc48657630814be
Theekshana Yugan <offmailacc2022@gmail.com>
2025-04-24 10:02:03 +0530

    fix button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,12 +16,10 @@
                             <input id="password" type="password" class="form-control" name="password" placeholder="Password *" required>
                         </div>
 
-                        <div class="form-group">
+                        <div class="form-group d-flex justify-content-between align-items-center">
                             <button type="submit" class="submit-button">
                                 LOGIN
                             </button>
-                        </div>
-                        <div class="form-group">
                             <a href="#">Forgot your password?</a>
                         </div>
                     </form>
