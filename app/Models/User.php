62699943a7f3ba5eab5b6baf018d6e7047e80681
KavinduAluthwaththa <kabaluthwaththa@std.foc.sab.ac.lk>
2025-07-28 09:18:56 +0530

    Refactor User model: replace 'student_id' with 'registration_number' in fillable attributes

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'student_id',
+        'registration_number',
         'role',
         'department',
         'phone',
