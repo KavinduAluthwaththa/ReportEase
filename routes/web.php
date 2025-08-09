0587d101ad49adfc81e4489966d38360863d96c8
harishihan-c <harishihan@outlook.com>
2025-08-10 04:11:10 +0530

    refactor: remove spaces and align the code

--- routes/web.php
+++ routes/web.php
@@ -33,8 +33,8 @@ Route::post('/register-custom',[AuthController::class, 'RegisterCustom'])->name(
 Route::get('/logout', [AuthController::class, 'Logout'])->name('logout');
 
 
+
 //Previous Reports
 Route::get('/', [ReportController::class, 'index'])->name('report');
-
 //See more page (from Previous report page)
 Route::get('/report/{id}', [ReportController::class, 'show'])->name('report.show');
