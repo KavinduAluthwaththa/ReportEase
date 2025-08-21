eca61d2f23b3b129c25d4b2e9c60bae0bd98a9a4
PBPerera <kpbperera655@gmail.com>
2025-08-22 00:44:59 +0530

    Update files to match and align with other pages

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>ReportEase</title>
-    <link rel="stylesheet" href="{{ asset('css/recovery.css') }}">
-   
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
